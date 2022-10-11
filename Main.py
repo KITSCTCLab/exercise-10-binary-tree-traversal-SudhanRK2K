ca1213927d5ee38bdcdbe02babe59c2a04039feb
SudhanRK2K <110010355+SudhanRK2K@users.noreply.github.com>
2022-10-11 20:51:14 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,40 +11,40 @@ def insert(root, new_value) -> BinaryTreeNode:
         If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
         Finally, return the root.
         """
-    # Write your code here
-    if root==None:
-        root=BinaryTreeNode(new)
+    if not root:
+        root = BinaryTreeNode(new_value)
         return root
+    if new_value < root.data:
+         if root.left_child:
+            insert(root.left_child, new_value)
+         else:
+            root.left_child = BinaryTreeNode(new_value)
     else:
-        if root.data > new:
-            if root.left_child is None:
-                node = BinaryTreeNode(new)
-                root.left_child = node
-            else:
-                insert(root.left_child,new)
+        if root.right_child:
+            insert(root.right_child, new_value)
         else:
-            if root.right_child is None:
-                node = BinaryTreeNode(new)
-                root.right_child = node
-            else:
-                insert(root.right_child,new)
+            root.right_child = BinaryTreeNode(new_value)
 
 
 def inorder(root) -> None:
-     if root:
-            inorder(root.left_child)
-            print(root.data, end = " ")
-            inorder(root.right_child)
+    if root is None:
+        return
+    inorder(root.left_child)
+    print(root.data, end = " ")
+    inorder(root.right_child)
 
 
 def preorder(root) -> None:
-    if root:
-        print(root.data, end = " ")
-        preorder(root.left_child)
-        preorder(root.right_child)
+    if root is None:
+        return
+    print(root.data, end = " ")
+    preorder(root.left_child)
+    preorder(root.right_child)
 
 
 def postorder(root) -> None:
+    if root is None:
+        return
     postorder(root.left_child)
     postorder(root.right_child)
     print(root.data, end = " ")
